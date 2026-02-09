72fc4134d03c2b348881d6ae4686b7c618944bfe
jessia <sadiajessia@gmail.com>
2026-02-09 16:19:28 +0600

    fetched and display users data using raw sql

--- GoStack/main.go
+++ GoStack/main.go
@@ -5,12 +5,22 @@ import (
   "fmt"
   "log"
   "os"
+  "time"
 
   "gorm.io/driver/postgres"
   "gorm.io/gorm"
   "github.com/joho/godotenv"
   _ "github.com/jackc/pgx/v5/stdlib"
 )
+
+type User struct {
+	ID  int `gorm:"primaryKey"`
+	Name  string
+	Email string
+	Phone string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
 func main () {
 
@@ -41,11 +51,25 @@ func main () {
 	gormDB, err := gorm.Open(postgres.New(postgres.Config{
 	Conn: sqlDB,
 	}), &gorm.Config{})
-	_ = gormDB 
+	
+	// _ = gormDB 
 
 	if err != nil {
 		log.Fatal("Failed to initialize gorm:", err)
 	}
 
 	fmt.Println("Successfully connected to the database using GORM with pgx driver!")
+
+	// fetch users using raw sql query
+	var users []User
+	gormDB.Raw("select * from users").Scan(&users)
+
+	for _, u := range users {
+		fmt.Printf("ID: %v, Name: %v, Email: %v, Phone: %v\n", u.ID, u.Name, u.Email, u.Phone)
+	}
+
+	// fetch user by id
+	var user User
+	gormDB.Raw("select * from users where id = ?", 1).Scan(&user)
+	fmt.Printf("User with ID 1: Name: %v, Email: %v, Phone: %v\n", user.Name, user.Email, user.Phone)
 }
