0f1dbde7228a3ba5fcd154eb32b98e9881da03b6
jessia <sadiajessia@gmail.com>
2026-02-14 15:58:58 +0600

    rabbitmq consumer and publisher in nodejs

---
+++ RabbitMQ/consumer.js
@@ -0,0 +1,22 @@
+const amqp = require("amqplib");
+
+connect()
+
+async function connect() {
+    try{
+
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+
+        channel.consume("jobs", message => {
+            console.log(message.content.toString());
+        })
+
+        console.log("Waiting for messages...");
+
+
+    } catch (err) {
+        console.log(err);
+    }
+}
---
+++ RabbitMQ/publisher.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib");
+
+const msg = {name: process.argv[2]};
+
+connect()
+
+async function connect() {
+    try{
+
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+
+        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+        console.log(`Message sent ${JSON.stringify(msg)}`);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
